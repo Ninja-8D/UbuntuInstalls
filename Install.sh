b23ebcc97c08fdae5c9e4ee47e9829319835a225
Ninja-8D <134347817+Ninja-8D@users.noreply.github.com>
2023-05-31 19:44:04 -0600

    Made Names consist to a convention
    ALLCAPS=Constants.  Also don't affect a program installation.
    
    iName = install flag
    Name = Name of object/class/namespace
    NameProp = property or value

--- Install.sh
+++ Install.sh
@@ -1,20 +1,19 @@
 FILE=./InstallVars.sh  #file that will overwrite Variables for different computers. You can keep a copy for each computer in your house and reuse this script.
 EDITOR=gedit  #Editor to be used for editting files
 
-#============================ NAS and network variables =====================================
+#============================ Nas and network variables =====================================
 #bash for install NFS and other apps.  Ubuntu 23.04 based distros
-SUBNET=""           #netwrok subnet of NAS
-NAS=""              #host address for NAS Sever
-PLEX=""             #host address for PLEX Server
+SubNet=""           #netwrok SubNet of Nas
+Nas=""              #host address for Nas Sever
+Plex=""             #host address for Plex Server
 
 #Flags for optionally installing required sections
-iNAS=true
-iPLEX=true
+iNas=true
+iPlex=true
 
-MOUNTPOINT="/mnt"  #every Linux machine will have this
-FOLDERS=("test" "new")   #array for folder names
+MountPoint="/mnt"  #every Linux machine will have this
+Folders=("test" "new")   #array for folder names
 
-EDITOR=gedit  #Editor to be used for editting files
 
 #==================== VSCODE ==========================
 iVSCODE=true #not Ready.  Files downloaded is not a debian structure file.  TODO:  Need to chown the VsCodeDeb file to not root.
@@ -31,37 +30,37 @@ if test -f "$FILE"; then  #check if file exists
 
 echo " ==================== Getting NFS Network and Folders variables  ========================"
 #Network section.  Fill in the values 
-if [$SUBNET -z ] ; then
-    echo "Enter your subnet. Example: 192.168.0"
-    read SUBNET 
+if [$SubNet -z ] ; then
+    echo "Enter your SubNet. Example: 192.168.0"
+    read SubNet 
     fi
 
-if [$NAS -z ] && $iNAS; then
-    echo "Enter host address for the NAS server.  Example 100" 
-    read NAS
+if [$Nas -z ] && $iNas; then
+    echo "Enter host address for the Nas server.  Example 100" 
+    read Nas
     fi
 
-if [$PLEX -z ] && $iPLEX  ; then
-    echo "Enter host address for the PLEX server.  Example 100"
-    read PLEX 
+if [$Plex -z ] && $iPlex  ; then
+    echo "Enter host address for the Plex server.  Example 100"
+    read Plex 
     fi
 
 #NFS Mount point
-if [$MOUNTPOINT -z ] && $iNAS ; then
-    echo "Enter the mount point for the NFS folders.  Example /mnt."
-    read MOUNTPOINT 
+if [$MountPoint -z ] && $iNas ; then
+    echo "Enter the mount point for the NFS Folders.  Example /mnt."
+    read MountPoint 
     fi
 
 
 
-echo " ==================== Creating NFS folders  ========================"
-echo $MOUNTPOINT && cd $MOUNTPOINT
-if $iNAS ; then
-    if [ ${#FOLDERS[@]} -gt 0 ] ; then
-        for f in ${FOLDERS[@]}; do
+echo " ==================== Creating NFS Folders  ========================"
+echo $MountPoint && cd $MountPoint
+if $iNas ; then
+    if [ ${#Folders[@]} -gt 0 ] ; then
+        for f in ${Folders[@]}; do
             mkdir $f ; done
     else 
-        echo "FOLDERS variable is empty" 
+        echo "Folders variable is empty" 
         fi
         
     #install the NFS package needed for the client side 
@@ -69,16 +68,16 @@ if $iNAS ; then
     sudo apt install nfs-kernel-server -y
     sudo apt install nfs-common -y
 
-    #check install and displays folders from NFS server
-    sudo showmount  -e $SUBNET"."$NAS
+    #check install and displays Folders from NFS server
+    sudo showmount  -e $SubNet"."$Nas
 
     #edit files for mounting.
-    sudo $EDITOR /etc/hosts  #set host for NAS before the fstab.  
+    sudo $EDITOR /etc/hosts  #set host for Nas before the fstab.  
 
-    sudo $EDITOR /etc/fstab  #Use the NAS host in the FSTAB
+    sudo $EDITOR /etc/fstab  #Use the Nas host in the FSTAB
     sudo showmount -a
     #sudo systemctl restart daemon-reload  
-    fi  #$iNAS
+    fi  #$iNas
     
     
 
@@ -90,10 +89,9 @@ if $iVsCode; then
     VsCodeCurl=$(curl $VsCode | grep -o -E 'code.+deb\"' | tail -1)  #GREP the last/latest deb file. There is an extra "
     VsCodeMatch=${VsCodeCurl::-1} #remove the last character
     
-    echo -e "\n========== URL: "$VsCodeMatch" ======================\n"
+    #echo -e "\n========== URL: "$VsCodeMatch" ======================\n"
     echo -e "\n========== URL: "$VsCodeUrl" ======================\n"
     wget -O $VsCodeDeb $VsCodeUrl && sudo dpkg -i $VsCodeDeb
     fi
     
 echo " ==================== DONE  ========================"
-
